d80e949a48e1ae459e6798c3d883c8e4171a8a4e
TirthVadaria <tirthvadaria@gmail.com>
2024-04-02 15:14:11 +0530

    socket url fixed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:4889",{
+			const socket = io("https://webchatapp-project.onrender.com",{
 				query : {
 					userId: authUser._id
 				}
